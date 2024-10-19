6cd4b7a4fa8d8eb8c4802aff4c97b5cb6fdd7faf
axelburks <axel.burks@gmail.com>
2024-10-19 16:41:13 +0800

    msgflow

---
+++ msgflow.py
@@ -0,0 +1,118 @@
+import os
+import yaml
+import logging
+import platform
+
+import rumps
+import darkdetect
+from apscheduler.triggers.interval import IntervalTrigger
+from apscheduler.schedulers.background import BackgroundScheduler
+
+from emailflow import EmailFlow
+from smsflow import SMSFlow
+
+rumps.debug_mode(True)
+
+class MSGFLOW(object):
+    def __init__(self):
+        self.sys_theme = darkdetect.theme()
+        icon_file = self.get_menu_icon()
+        self.app = rumps.App('MSGFLOW', icon = icon_file)
+        self.task_status = rumps.MenuItem(title = '- 监听状态：开启', callback = None)
+        self.notification_status = rumps.MenuItem(title = '- 通知状态：开启', callback = None)
+        self.clipboard_status = rumps.MenuItem(title = '- 剪切板状态：开启', callback = None)
+        self.task_button = rumps.MenuItem(title = '开/关 监听任务', callback = self.switch_task)
+        self.notification_button = rumps.MenuItem(title = '开/关 通知', callback = self.switch_notification)
+        self.clipboard_button = rumps.MenuItem(title = '开/关 剪切板', callback = self.switch_clipboard)
+        self.task_status.state = 1
+        self.notification_status.state = 1
+        self.clipboard_status.state = 1
+        self.app.menu = [self.task_status, self.notification_status, self.clipboard_status, self.task_button, self.notification_button, self.clipboard_button]
+        self.scheduler = BackgroundScheduler()
+        self.app_scheduler = BackgroundScheduler()
+        self.email_list = []
+
+    def switch_task(self, _):
+        if self.task_status.state == 1:
+            self.task_status.title = '- 监听状态：关闭'
+            self.task_status.state = 0
+            self.switch_notification('off')
+            self.switch_clipboard('off')
+            self.scheduler.pause()
+        else:
+            self.task_status.title = '- 监听状态：开启'
+            self.task_status.state = 1
+            self.switch_notification('on')
+            self.switch_clipboard('on')
+            self.scheduler.resume()
+
+    def switch_notification(self, aciton_type = None):
+        if self.notification_status.state == 1 or aciton_type == 'off':
+            self.notification_status.title = '- 通知状态：关闭'
+            self.notification_status.state = 0
+            self.smsflow.notify.status = 0
+            for i in self.email_list:
+                i.notify.status = 0
+        else:
+            self.notification_status.title = '- 通知状态：开启'
+            self.notification_status.state = 1
+            self.smsflow.notify.status = 1
+            for i in self.email_list:
+                i.notify.status = 1
+    
+    def switch_clipboard(self, aciton_type = None):
+        if self.clipboard_status.state == 1 or aciton_type == 'off':
+            self.clipboard_status.title = '- 剪切板状态：关闭'
+            self.clipboard_status.state = 0
+            self.smsflow.clipboard.status = 0
+            for i in self.email_list:
+                i.clipboard.status = 0
+        else:
+            self.clipboard_status.title = '- 剪切板状态：开启'
+            self.clipboard_status.state = 1
+            self.smsflow.clipboard.status = 1
+            for i in self.email_list:
+                i.clipboard.status = 1
+            
+    def get_menu_icon(self):
+        os_version = platform.platform().split('-')[1].replace('.', '')
+        if int(os_version) < 1016:
+            return os.path.join('statics', 'light.png' if self.sys_theme == 'Dark' else 'dark.png')
+        else:
+            return os.path.join('statics', 'light.png')
+    
+    def update_menu_icon(self):
+        if self.sys_theme != darkdetect.theme():
+            self.sys_theme = darkdetect.theme()
+            self.app.icon = self.get_menu_icon()
+    
+    def run(self):
+        with open(os.path.expanduser('~/config.yaml'), 'r') as fp:
+            config = yaml.safe_load(fp)
+        db_file = os.path.expanduser('~/Library/Messages/chat.db')
+
+        trigger = IntervalTrigger(seconds=3)
+        if 'email' in config:
+          for i in config['email']:
+              self.email_list.append(EmailFlow(i['username'], i['password'], i.get('pop_server', 'pop.' + i['username'].split('@')[1])))
+          for i in self.email_list:
+              self.scheduler.add_job(i.update_hook, trigger)
+        self.smsflow = SMSFlow(db_file)
+        self.scheduler.add_job(self.smsflow.update_hook, trigger)
+        self.scheduler.start()
+
+        trigger = IntervalTrigger(seconds=2)
+        self.app_scheduler.add_job(self.update_menu_icon, trigger)
+        self.app_scheduler.start()
+
+        self.app.run()
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level = logging.INFO, format = '%(asctime)s - %(processName)s - %(lineno)d - %(name)s - %(levelname)s - %(message)s')
+    logger = logging.getLogger(__name__)
+    logging.getLogger("apscheduler.executors.default").setLevel(logging.ERROR)
+
+    app = MSGFLOW()
+    app.run()
+
---
+++ smsflow.py
@@ -0,0 +1,100 @@
+import os
+import re
+import time
+import logging
+import sqlite3
+
+from base import Base
+
+class LiteDB(object):
+    def __init__(self, db_file):
+        self.db_file = db_file
+    
+    def dict_factory(self, cursor, row): 
+        dict_c = {} 
+        for idx, col in enumerate(cursor.description): 
+            dict_c[col[0]] = row[idx] 
+        return dict_c
+
+    def conn(self):
+        conn = sqlite3.connect(self.db_file)
+        conn.row_factory = self.dict_factory
+        cursor = conn.cursor()
+        return conn, cursor
+    
+    def select(self, sql):
+        conn, cursor = self.conn()
+        result = list(cursor.execute(sql))
+        conn.commit()
+        conn.close()
+        return result
+
+
+class SMSFlow(Base):
+    def __init__(self, db_file):
+        super(SMSFlow, self).__init__()
+        self.db_file = db_file
+        self.db = LiteDB(self.db_file)
+        self.update_time = int(time.time())
+    
+    def get_message(self):
+        sql = """
+        select
+            message.rowid,
+            ifnull(handle.uncanonicalized_id, chat.chat_identifier) AS sender,
+            message.service,
+            (message.date / 1000000000 + 978307200) AS message_date,
+            message.text
+        from
+            message
+                left join chat_message_join
+                        on chat_message_join.message_id = message.ROWID
+                left join chat
+                        on chat.ROWID = chat_message_join.chat_id
+                left join handle
+                        on message.handle_id = handle.ROWID
+        where
+            is_from_me = 0
+            and text is not null
+            and length(text) > 0
+            and (
+                text glob '*[0-9][0-9][0-9][0-9]*'
+                or text glob '*[0-9][0-9][0-9][0-9][0-9]*'
+                or text glob '*[0-9][0-9][0-9][0-9][0-9][0-9]*'
+                or text glob '*[0-9][0-9][0-9][0-9][0-9][0-9][0-9]*'
+                or text glob '*[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]*'
+            )
+        order by
+            message.date desc
+        limit 10
+        """
+        data = self.db.select(sql)
+        return data
+    
+    def get_code(self):
+        self.logging.debug('select db')
+        msgs = self.get_message()
+        result = []
+        for i in msgs:
+            if 'code' in i['text'] or '码' in i['text']:
+                i['code'] = re.findall(r'([0-9]{4,8})', i['text'])
+                if i['code']:
+                    i['code'] = i['code'][0]
+                    result.append(i)
+        return result
+    
+    def _notify(self):
+        temp = self.get_code()[0]
+        self.logging.debug(temp)
+        if int(time.time()) - int(temp['message_date']) < 30:
+            self.notification(temp['code'], temp['text'])
+            self.notify_to_bark(temp['code'], temp['text'], temp['code'])
+            self.save_to_clipboard(temp['code'])
+    
+    def update_hook(self):
+        self.logging.debug('checking')
+        update_time = int(os.path.getmtime(self.db_file))
+        tp = abs(update_time - self.update_time)
+        if not tp and tp <= 15:
+            self.update_time = update_time
+            self._notify()
